6c96c5641ede5e225f7a3b9ed13887b26ff666a6
Pravar Anu <pravaranu6@gmail.com>
2023-01-27 21:00:38 +0530

    Creating constructor and setting a variable for name

--- src/App.js
+++ src/App.js
@@ -3,22 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      name: "Pravar"
+    }
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Hi I'm {this.state.name}.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          
         </header>
       </div>
     );
