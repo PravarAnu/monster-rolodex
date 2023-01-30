6a453870016bc963a2b22d73f5029cbac32b4105
Pravar Anu <pravaranu6@gmail.com>
2023-01-30 22:07:54 +0530

    Explained how is the flow of class component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,21 @@ import { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  /* 
+    Process of running of the class
+
+    1.constructor
+    2.render
+    3.componentDidMount
+    4.render
+
+
+    Here the constructor runs first.Now we know that there is a state named monster
+    then the render function runs which makes the skeleton of the page by using HTML.
+    then the componentDidMount runs which fetch the data from the api and setState of
+    monsters. As the setState process ends the react recognise and render the skeleton 
+    again with updated values of monsters.
+  */
   constructor(){
     super();
 
