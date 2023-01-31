13411afd78417d8ddc673ebde93c27a4ac0b61d1
Pravar Anu <pravaranu6@gmail.com>
2023-01-31 19:46:14 +0530

    Done some optimization

--- src/App.js
+++ src/App.js
@@ -38,17 +38,22 @@ class App extends Component {
       });
   }
 
+  onSearchChange = (event)=>{
+    const searchField = event.target.value;
+    this.setState({searchField});
+  };
+
   render() {
-    const filteredMonsters = this.state.monsters.filter((monster) =>{
-      return monster.name.toLowerCase().includes(this.state.searchField);
+    const {monsters, searchField} = this.state;
+    const {onSearchChange} = this;
+
+    const filteredMonsters = monsters.filter((monster) =>{
+      return monster.name.toLowerCase().includes(searchField);
     });
 
     return (
       <div className="App">
-        <input type="search" placeholder='Search Monsters' onChange={(event)=>{
-          const searchField = event.target.value;
-          this.setState({searchField});
-        }}/>
+        <input type="search" placeholder='Search Monsters' onChange={onSearchChange}/>
         {
           filteredMonsters.map((monster) => {
             return <h1 key={monster.id}>{monster.name}</h1>
