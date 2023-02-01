67188a0d7e4e9f8de5375f95e874d9cc0de30d7c
Pravar Anu <pravaranu6@gmail.com>
2023-02-01 22:27:35 +0530

    changed the class component to functional component

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
     setFilteredMonsters(newFilteredMonsters);
   }, [monsters, searchField]);
 
-  
+
   const onSearchChange = (event) => {
     const searchFieldString = event.target.value;
     setSearchField(searchFieldString);
@@ -46,70 +46,6 @@ function App() {
     </div>
   )
 }
-// class App extends Component {
-//   /* 
-//     Process of running of the class
 
-//     1.constructor
-//     2.render
-//     3.componentDidMount
-//     4.render
-
-
-//     Here the constructor runs first.Now we know that there is a state named monster
-//     then the render function runs which makes the skeleton of the page by using HTML.
-//     then the componentDidMount runs which fetch the data from the api and setState of
-//     monsters. As the setState process ends the react recognise and render the skeleton 
-//     again with updated values of monsters.
-//   */
-//   constructor(){
-//     super();
-
-//     this.state = {
-//       monsters: [],
-//       searchField: ''
-//     }
-//   }
-
-//   componentDidMount(){
-// fetch("https://jsonplaceholder.typicode.com/users#")
-//   .then((response) =>response.json())
-//   .then((users) =>{
-//     this.setState(
-//       () =>{
-//         return {monsters: users};
-//       }
-//     )
-//   });
-//   }
-
-//   onSearchChange = (event)=>{
-//     const searchField = event.target.value;
-//     this.setState({searchField});
-//   };
-
-//   render() {
-//     const {monsters, searchField} = this.state;
-//     const {onSearchChange} = this;
-
-// const filteredMonsters = monsters.filter((monster) =>{
-//   return monster.name.toLowerCase().includes(searchField);
-// });
-
-//     return (
-//       <div className="App">
-//         <h1 className='app-title'>Monster Rolodex</h1>
-//         <SearchBox
-//           className = "search-box"
-//           onChangeHandler={onSearchChange} 
-//           placeholder="Search-Monster"
-//         />
-//         <CardList 
-//           monsters={filteredMonsters}
-//         />
-//       </div>
-//     );
-//   }
-// }
 
 export default App;
